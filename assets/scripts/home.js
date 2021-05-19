a1875301d36ccfddad95409fa9c968189e069273
JulGeff <julien.geffriaud.pro@gmail.com>
2021-05-19 21:23:21 +0200

    test home.js

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,10 +1,29 @@
-document.getElementById('jstest').innerHTML = '<a href="#">New text!</a>';
+fetch('http://localhost:3000/api/cameras')
+    .then((response) => response.json())
+    .then(function(cameras) {
+        addCameras(cameras)
+     })
+
+function addCameras(cameras) {
+    cameras.forEach(function(camera) {
+        displayCamera(camera);
+    })
+}
+
+function displayCamera(camera) {
+    document.getElementById('camera1__name').innerHTML = camera[1].lenses;
+    // creation de ton html pour injecter une camera dans le tamplate
+    // Toujours avec le même princique que tu as fais pour ajouter les element dans ton html
+    
+}
 
 
 
 
 
+//test modif texte bouton 1 au clic
+var elt = document.getElementsByClassName('buttons')[0];
+elt.addEventListener('click', function () {          // On écoute l'événement click
+        elt.innerHTML = "Ajouté à votre panier";
 
-
-
-
+})
--- assets/scripts/product.js
+++ assets/scripts/product.js
@@ -1,5 +1,9 @@
-
+// TEST JS
 var elt = document.getElementsByClassName('buttons')[0];
 elt.addEventListener('click', function () {          // On écoute l'événement click
-        elt.innerHTML = "Ajouté à votre panier";               // On change le contenu de notre élément pour afficher "C'est cliqué !"
+        elt.innerHTML = "Ajouté à votre panier";     // On change le contenu de notre élément 
     });
+// FIN TEST
+
+
+        
