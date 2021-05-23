74d5abf71ae69a3a8ff185b573350c57734342d2
JulGeff <julien.geffriaud.pro@gmail.com>
2021-05-24 00:03:47 +0200

    product.js v3

--- assets/scripts/product.js
+++ assets/scripts/product.js
@@ -1,14 +1,17 @@
-let id = location.search.substr(4);
+
+let id = location.search.substr(4); //extraction de l'ID depuis l'url
 console.log (id);
 let main = document.getElementById('main');
+
 
 fetch('http://localhost:3000/api/cameras')
     .then((response) => response.json())
     .then(function (cameras) {
         for (var i = 0; i < cameras.length; i++) {
             console.log (cameras[i]._id); 
-            if (id === cameras[i]._id) {
+            if (id === cameras[i]._id) { //comparaison de l'ID situé dans l'url avec les ID du fichier json issu de l'API
                 
+                // Quand ID correspondant avec clui de l'URL, insertion de la fiche produit
                 main.innerHTML += `
                 <h1>Craquez pour le ${cameras[i].name}!</h1>
                 <section>
@@ -19,12 +22,9 @@ fetch('http://localhost:3000/api/cameras')
                             <p>${cameras[i].description}</p>
 
                             <label for="options">Lentilles</label>
-
                             <select name="lenses" id="lenses-select">
                                 <option value="">--choisissez une option--</option>
-                                <option value="lense_1">${cameras[i].lenses[0]}</option>
-                                <option value="lense_2">${cameras[i].lenses[1]}}</option>
-                                <option value="lense_3">${cameras[i].lenses[2]}</option>
+                                
                             </select>
 
                             <p>Prix : ${cameras[i].price / 1000} €</p>
@@ -36,6 +36,24 @@ fetch('http://localhost:3000/api/cameras')
                     </div>
                 </li>`
 
+                // Insertion des options de lenses pour chaque appareil 
+                cameras[i].lenses.forEach((lens) => {
+                    function displayLensOption(lens) {
+                        let lensesOptions = document.getElementById('lenses-select');
+                                              
+                            lensesOptions.innerHTML += 
+                            `<option>${cameras[i].lenses.lens}</option>`
+                        };
+                        displayLensOption()
+                })
             }}});
 
 
+           
+    
+         
+ 
+            
+                
+                      
+            
