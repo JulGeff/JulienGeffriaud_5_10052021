cd4e20c6a5de632ec66eff131b96ef584f4cae60
JulGeff <julien.geffriaud.pro@gmail.com>
2021-05-23 22:40:09 +0200

    product.js v2

--- assets/scripts/product.js
+++ assets/scripts/product.js
@@ -1,42 +1,41 @@
-let prodid = location.search.substr(1);
+let id = location.search.substr(4);
+console.log (id);
 let main = document.getElementById('main');
 
 fetch('http://localhost:3000/api/cameras')
     .then((response) => response.json())
     .then(function (cameras) {
         for (var i = 0; i < cameras.length; i++) {
-            if (cameras[i]._id = prodid) {
-                main.innerHTML += `
+            console.log (cameras[i]._id); 
+            if (id === cameras[i]._id) {
                 
-        <h1>Craquez pour le ${cameras[i].name}!</h1>
-        <section>
-            <div class=product>
-                <img src="../images/vcam_${i + 1}.jpg" alt="appareil photo">
-                <div class=product__subtitle>
-                    <h2>${cameras[i].name}</h2>
-                    <p>${cameras[i].description}</p>
+                main.innerHTML += `
+                <h1>Craquez pour le ${cameras[i].name}!</h1>
+                <section>
+                    <div class=product>
+                        <img src="../images/vcam_${i + 1}.jpg" alt="appareil photo">
+                        <div class=product__subtitle>
+                            <h2>${cameras[i].name}</h2>
+                            <p>${cameras[i].description}</p>
 
-                    <label for="options">Lentilles</label>
+                            <label for="options">Lentilles</label>
 
-                    <select name="lenses" id="lenses-select">
-                        <option value="">--choisissez une option--</option>
-                        <option value="lense_1">${cameras[i].lenses[0]}</option>
-                        <option value="lense_2">${cameras[i].lenses[1]}}</option>
-                        <option value="lense_3">${cameras[i].lenses[2]}</option>
-                    </select>
+                            <select name="lenses" id="lenses-select">
+                                <option value="">--choisissez une option--</option>
+                                <option value="lense_1">${cameras[i].lenses[0]}</option>
+                                <option value="lense_2">${cameras[i].lenses[1]}}</option>
+                                <option value="lense_3">${cameras[i].lenses[2]}</option>
+                            </select>
 
-
-                    <p>Prix : ${cameras[i].price / 1000} €</p>
-                    <p>Référence produit : ${cameras[i]._id}</p>
-                    
-                    <button type="button" class="buttons">
-                        Ajouter au panier
-                    </button>
-                </div>
-            </div>
-        </li>`
+                            <p>Prix : ${cameras[i].price / 1000} €</p>
+                            
+                            <button type="button" class="buttons">
+                                Ajouter au panier
+                            </button>
+                        </div>
+                    </div>
+                </li>`
 
             }}});
-
 
 
