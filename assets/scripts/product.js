57bdf8e030ae624232ed7dc093b40759d9b537f8
JulGeff <julien.geffriaud.pro@gmail.com>
2021-05-25 16:20:49 +0200

    product.js v5

--- assets/scripts/product.js
+++ assets/scripts/product.js
@@ -1,74 +1,116 @@
 
 let id = location.search.substr(4); //extraction de l'ID depuis l'url
-console.log (id);
+console.log(id);
 let main = document.getElementById('main');
 
-
-fetch('http://localhost:3000/api/cameras')
+fetch(`http://localhost:3000/api/cameras/${id}`) // récupération de l'ID situé dans l'URL depuis l'API
     .then((response) => response.json())
-    .then(function (cameras) {
-        for (var i = 0; i < cameras.length; i++) {
-            console.log (cameras[i]._id); 
-            if (id === cameras[i]._id) { //comparaison de l'ID situé dans l'url avec les ID du fichier json issu de l'API
-                
-                // Quand ID correspondant avec clui de l'URL, insertion de la fiche produit ci-dessous
-                main.innerHTML += `
-                <h1>Craquez pour le ${cameras[i].name}!</h1>
-                <section>
-                    <div class=product>
-                        <img src="../images/vcam_${i + 1}.jpg" alt="appareil photo">
-                        <div class=product__subtitle>
-                            <h2>${cameras[i].name}</h2>
-                            <p>${cameras[i].description}</p>
+    .then(function (camera) {
+        console.log(camera);
+        productPage(camera);  // création de la fiche produit
+        lensesOptions(camera);// affichage des options de lenses
+        storeQt();             // au clic sur "Ajouter au panier", enregistrement clé id / qt dans local storage
+    })
 
-                            <p>
-                            <label for="options">Lentilles : </label>
-                            <select name="lenses" id="lenses-select">
-                                <option value="">--choisissez une option--</option>
-                                
-                            </select>
-                            </p>
-                            <p>
-                            <label for="q">Quantité : </label>
-                            <select id="qt" name="q">
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                <option value="6">6</option>
-                                <option value="7">7</option>
-                                <option value="8">8</option>
-                                <option value="9">9</option>
-                            </select>
-                            </p>
-                            <button type="button" div=${cameras[i].id}>
-                                Ajouter au panier
-                            </button>
-                        </div>
-                    </div>
-                </li>`
+//FONCTIONS APPELEES
+// création de la fiche produit
+function productPage(camera) {
+    main.innerHTML += `
+        <h1>Craquez pour le ${camera.name}!</h1>
+        <section>
+            <div class=product>
+                <img src="../images/vcam_1.jpg" alt="appareil photo">
+                <div class="product__subtitle" id="product__subtitle">
+                    <h2>Description du ${camera.name}</h2>
+                    <p>${camera.description}</p>
+                    <p>
+                        <label for="lenses">Lentilles : </label>
+                        <select name="lenses" id="lenses-select">
+                            <option value="" selected>--choisissez une option--</option> 
+                        </select>
+                    </p>
+                    <p>
+                        <label for="qt">Quantité : </label>
+                        <select id="qt" name="q">
+                            <option value="1" selected>1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                        </select>
+                    </p>
+                    <button type="button" id="button">
+                        Ajouter au panier
+                    </button>
+                </div>
+            </div>
+        </li>`
+}
+
+// Insertion des options de lenses pour chaque appareil 
+function lensesOptions(camera) {
+    camera.lenses.forEach((lens, index) => {
+        function displayLensOption() {
+            let lensesOptions = document.getElementById('lenses-select');
+            lensesOptions.innerHTML +=
+                `<option>${camera.lenses[index]}</option>`
+        };
+
+        displayLensOption();
+
+    })
+}
 
 
-                // Insertion des options de lenses pour chaque appareil 
-                cameras[i].lenses.forEach((lens, index) => {
-                    function displayLensOption(lens, index) {
-                        let lensesOptions = document.getElementById('lenses-select');    
-                            lensesOptions.innerHTML += 
-                            `<option>${cameras[i].lenses[index]}</option>`
-                        };
-                        displayLensOption(lens, index)
-                })
-            }}});
+//AJOUT AU PANIER
+// Récupération option choisie
+function chosenOption(option) {
+    let opt = document.getElementById("lenses-select");
+    opt.addEventListener('change', function () {
+        let option = opt[opt.selectedIndex].text;
+        console.log("Option choisie : " + option);
+  
 
 
+    })
+};
 
 
-           
+// Récupération quantité choisie
+function chosenQt() {
+    let qt = document.getElementById("qt");
+    qt.addEventListener('change', function () {
+        let quantity = qt.selectedIndex + 1;
+        console.log("Quantité choisie : " + quantity);
+
+    })
+};
+
+
+// Stockage de l'ID, option et de la quantité associée dans localStorage
+function storeQt(quantity, option) {
+chosenQt();
+chosenOption();
+
+
+    let product = {
+        id: id,
+        option: option,
+        quantity: quantity,
+    }
     
-         
- 
-            
-                
-                      
-            
+
+    const button = document.getElementById('button');                  // On récupère l'élément sur lequel on veut détecter le clic
+    button.addEventListener('click', function () { 
+        console.log(product);                    // On écoute l'événement click
+        window.localStorage.setItem(id, JSON.stringify(product)); // On stocke la chaîne id option / qt dans le local storage
+        let prod = document.getElementById("product__subtitle"); // On écrit "Produit ajouté au panier !" sous le bouton
+        let newp = document.createElement("p");
+        prod.appendChild(newp).innerHTML = "Produit ajouté au panier";
+    })
+};
+
